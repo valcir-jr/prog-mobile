ea17258286d9877d350e39077a28f149d8b8b6c5
valcir-jr <162353778+valcir-jr@users.noreply.github.com>
2025-09-25 22:54:43 -0300

    Delete prog-mobile directory

--- prog-mobile/services/cep.ts
+++
@@ -1,7 +0,0 @@
-import api from "./api";
-import { CepProps, NotFound } from "../types/cep";
-
-export async function get(cep: number): Promise<CepProps | NotFound> {
-  const { data } = await api.get(`/${cep}/json/`);
-  return data;
-};
